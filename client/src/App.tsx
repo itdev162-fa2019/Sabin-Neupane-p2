36c964b81f544dab7d5ed761a8198b70bb02e374
neupanesabin <54862359+neupanesabin@users.noreply.github.com>
2019-12-11 11:14:02 -0600

    Project-2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,112 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom";
+import PostList from "./components/PostList/PostList";
+import Post from "./components/Post/Post";
+import CreatePost from './components/Post/CreatePost';
+import EditPost from './components/Post/EditPost';
 
 
 class App extends React.Component {
   state = {
-   
-    posts: []
-  }
+    posts: [],
+    post: null
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-    axios.get('http://localhost:5000/api/posts')
-      .then((response) => {
-        this.setState({
-         
-          posts: response.data
-        })
-      })
-      .catch((error) => {
-        console.error(`Error fetching data: ${error}`);
-      })
-  }
-  render() {
+    axios
+      .get("http://localhost:5000/api/posts")
 
-    const { posts } = this.state;
+      .then(response => {
+        this.setState({
+          posts: response.data
+        });
+      })
+      .catch(error => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }
+  viewPost = post => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  };
+  deletePost = post => {
+    axios
+      .delete(`http://localhost:5000/api/posts/${post.id}`)
+      .then(response => {
+        const newPosts = this.state.posts.filter(p => p.id !== post.id);
+        this.setState({
+          posts: [...newPosts]
+        });
+      })
+
+      .catch(error => {
+        console.error(`Error deleting post: ${error}`);
+      });
+  };
+  editPost = post => {
+    this.setState({
+      post: post
+    });
+  };
+  onPostCreated = post => {
+    const newPosts = [...this.state.posts, post];
+
+    this.setState({
+      posts: newPosts
+    });
+  };
+
+  onPostUpdated = post => {
+    console.log('updated post: ', post);
+    const newPosts = [...this.state.posts];
+    const index = newPosts.findIndex(p => p.id === post.id);
+    newPosts[index] = post;
+    this.setState({
+      posts: newPosts
+    });
+  };
+
+  render() {
+    const { posts, post } = this.state;
 
     return (
-      <div className="App">
-        <header className="App-header">
-          BlogBox
-        </header>
-        
-        <main>
-          {posts.map((post: any) =>
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-            </div>
-          )}
-        </main>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">BlogBox</header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/new-post"> New Post</Link>
+          </nav>
+          <main className="App-content">
+            <switch>
+              <Route exact path="/">
+                <PostList
+                  posts={posts}
+                  clickPost={this.viewPost}
+                  deletePost={this.deletePost}
+                  editPost={this.editPost}
+                />
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
+              <Route path="/new-post">
+                <CreatePost onPostCreated={this.onPostCreated} />
+              </Route>
+              <Route path="./edit-post/:postId">
+                <EditPost post={post} onPostUpdated={this.onPostUpdated} />
+              </Route>
+            </switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
+
+
 export default App;
